5eb92ede01049eb0f4bf00d267608a4c282e874c
J <knowisdom314@gmail.com>
2025-05-28 17:06:12 +0900

    8 - 프로젝트 구조 정리 및 파일 리팩토링

---
+++ Kartrider/Navigation/AppNavigationView.swift
@@ -0,0 +1,18 @@
+//
+//  AppNavigationView.swift
+//  Kartrider
+//
+//  Created by J on 5/28/25.
+//
+
+import SwiftUI
+
+struct AppNavigationView: View {
+    var body: some View {
+        Text("ContentView에 있는 NavgationStack 관련된 거 다 여기로 옮겨주세요.")
+    }
+}
+
+#Preview {
+    AppNavigationView()
+}
---
+++ Kartrider/Navigation/NavigationCoordinator.swift
@@ -0,0 +1,12 @@
+//
+//  NavigationCoordinator.swift
+//  Kartrider
+//
+//  Created by J on 5/28/25.
+//
+
+import Foundation
+
+class NavigationCoordinator: ObservableObject {
+    
+}
