1014b99a09f3f381b44554bb6f64d2e60696bb78
J <knowisdom314@gmail.com>
2025-05-31 23:52:59 +0900

    #19 - 캐러셀 카드 뷰 컴포넌트화

---
+++ Kartrider/Componenets/Cards/ContentCardView.swift
@@ -0,0 +1,51 @@
+//
+//  StoryCardView.swift
+//  Kartrider
+//
+//  Created by J on 5/31/25.
+//
+
+import SwiftUI
+
+struct ContentCardView: View {
+    let content: ContentMeta
+    
+    private var thumbnailImage: Image {
+        guard let name = content.thumbnailName, UIImage(named: name) != nil else {
+            return Image("default_thumbnail")
+        }
+        return Image(name)
+//        Image("default_thumbnail")
+//            .resizable()
+//            .scaledToFill()
+//            .frame(width: 240, height: 320)
+//            .background(Color.secondary)
+    }
+    
+    var body: some View {
+        ZStack(alignment: .topLeading) {
+            thumbnailImage
+                .resizable()
+                .scaledToFill()
+                .frame(width: 300, height: 480)
+                .cornerRadius(16)
+                .overlay (
+                    RoundedRectangle(cornerRadius: 16)
+                        .stroke(Color.divider, lineWidth: 1)
+                )
+            
+            HStack(spacing: 8) {
+                ForEach(content.hashtags, id:\.self ) { tag in
+                    TagBadgeView(text: tag, style: .primary)
+                }
+            }
+            .padding(16)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentCardView(content: ContentMeta(title: "눈 떠보니 내가 T1 페이커?!", summary: "오늘이 MSI 결승인데, 아이언인 내가 페이커 몸에 들어와버림", type: .story, hashtags: ["페이커", "빙의", "큰일남"], thumbnailName: ""))
+    .padding()
+}
