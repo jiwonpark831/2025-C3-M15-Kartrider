8327476b8cabca844fb505169605a8bb70842dc4
jiwonpark831 <jiwonpark831@gmail.com>
2025-06-01 15:21:51 +0900

    watch folder 구조 수정

---
+++ Pickacha Watch App/WatchFeature/Start/WatchStartViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  WatchStartViewModel.swift
+//  Pickacha Watch App
+//
+//  Created by jiwon on 6/1/25.
+//
+
+import Foundation
--- Pickacha Watch App/WatchViewModel/WatchViewModel.swift
+++
@@ -1,8 +0,0 @@
-//
-//  WatchViewModel.swift
-//  Pickacha Watch App
-//
-//  Created by jiwon on 5/31/25.
-//
-
-import Foundation
