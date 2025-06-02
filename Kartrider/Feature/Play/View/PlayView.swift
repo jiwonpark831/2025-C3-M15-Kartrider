487b50ef498d23fbbf25a18618023b0e35760d32
J <knowisdom314@gmail.com>
2025-06-02 18:24:56 +0900

    #32 - DescriptionBox 좌측 정렬

---
+++ Kartrider/Feature/Play/View/PlayView.swift
@@ -0,0 +1,18 @@
+//
+//  PlayView.swift
+//  Kartrider
+//
+//  Created by J on 6/2/25.
+//
+
+import SwiftUI
+
+struct PlayView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PlayView()
+}
