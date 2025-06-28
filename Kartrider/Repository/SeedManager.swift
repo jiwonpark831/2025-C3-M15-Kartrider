cad66adccd6ffae1a279dbcdb6061fe39ee87e01
J <knowisdom314@gmail.com>
2025-06-28 16:56:01 +0900

    refactor: Seeder 및 JSONParser 구조 통합

--- Kartrider/Repository/SeedManager.swift
+++ Kartrider/Repository/SeedManager.swift
@@ -11,6 +11,7 @@ import SwiftData
 @MainActor
 class SeedManager: ObservableObject {
     @Published var isReady = false
+    
     let container: ModelContainer
     
     init() {
@@ -29,7 +30,7 @@ class SeedManager: ObservableObject {
         
         let config = ModelConfiguration(
             schema: schema,
-            isStoredInMemoryOnly: true // TODO: - 나중에 false로 변경
+            isStoredInMemoryOnly: true // TODO: - false로 변경
         )
         
         do {
@@ -38,74 +39,50 @@ class SeedManager: ObservableObject {
             fatalError("Could not create ModelContainer: \(error)")
         }
     }
-    // TODO: 제발 개행에 신경을 쓰기
+    
     func seedIfNeeded() async {
         let context = container.mainContext
         let resetKey = "hasSeededOnce"
-        
         let isInMemory = container.configurations.first?.isStoredInMemoryOnly == true
         
-        // MARK: - 개발용 강제 초기화 트리거 (배포 전 삭제)
         #if DEBUG
         if ProcessInfo.processInfo.environment["RESET_DB"] == "true" { // TODO: - 배포할 때 스키마 RESET_DB = false로 변경
             print("[DEBUG] RESET_DB 환경 변수 감지")
-            await resetAll(context: context)
-            // TODO: UserDefaults 관리하는 객체 추가.
+            await Seeder<StoryJSON>.deleteAll(context: context)
+            await Seeder<TournamentJSON>.deleteAll(context: context)
             UserDefaults.standard.removeObject(forKey: resetKey)
         }
         #endif
         
-        // isInMemory가 true일 경우 매번 시드
         if isInMemory {
             print("[DEBUG] 인메모리 모드 -> 무조건 시드")
             await performSeeding(context: context)
+            isReady = true
             return
         }
         
-        // 최초 실행 여부 체그 (영구 저장인 경우만)
         let hasSeeded = UserDefaults.standard.bool(forKey: resetKey)
         print("[DEBUG] hasSeededOnce 값: \(hasSeeded)")
         
         guard !hasSeeded else {
-            isReady = true
             print("[DEBUG] 이미 시드 완료됨 -> 종료")
+            isReady = true
             return
         }
         
         print("[INFO] 최초 실행 → 시드 시작")
         await performSeeding(context: context)
         UserDefaults.standard.set(true, forKey: resetKey)
+        isReady = true
     }
-    
-    // MARK: - 전체 삭제 (개발용)
-    // TODO: 동시성 문제 해결!
-    private func resetAll(context: ModelContext) async {
-        do {
-            try await deleteAll(of: StoryStep.self, context: context)
-            try await deleteAll(of: TournamentStep.self, context: context)
-            try await deleteAll(of: PlayHistory.self, context: context)
-            try await deleteAll(of: ContentMeta.self, context: context)
 
-            try? context.save()
-        } catch {
-            print("[ERROR] SwiftData 초기화 중 오류 발생: \(error)")
-        }
-    }
-    
-    private func deleteAll<T: PersistentModel>(of type: T.Type, context: ModelContext) async throws {
-        let all = try context.fetch(FetchDescriptor<T>())
-        for item in all {
-            context.delete(item)
-        }
-    }
-    
     private func performSeeding(context: ModelContext) async {
-        await StorySeeder.seedIfNeeded(context: context)
-        await TournamentSeeder.seedIfNeeded(context: context)
+        await Seeder<StoryJSON>.seedAll(context: context)
+        await Seeder<TournamentJSON>.seedAll(context: context)
 
         try? await Task.sleep(nanoseconds: 1_500_000_000) // 1.5초
-
         isReady = true
+        
         print("[DEBUG] 시드 완료 → isReady = true")
     }
 }
